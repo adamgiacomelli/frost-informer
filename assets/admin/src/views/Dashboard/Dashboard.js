bbaad37fa3ec3443ab4fda14b86d3dda7e59922a
adam_giacomelli <adam.giacom@gmail.com>
2017-08-21 14:09:01 +0200

    Added administration dashboard

---
+++ assets/admin/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Welcome to the CherryDeck Admin
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
