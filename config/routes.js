5eb8151b489a25d24990d8ebd073c6b698aba615
Ziga Kerec <ziga.kerec@gmail.com>
2017-08-24 15:16:35 +0200

    refactoring routes

--- config/routes.js
+++ config/routes.js
@@ -20,7 +20,12 @@
  * http://sailsjs.org/#!/documentation/concepts/Routes/RouteTargetSyntax.html
  */
 
-module.exports.routes = {
+const meRoutes = require('./routes/meRoutes');
+const invitationRoutes = require('./routes/invitationRoutes');
+const authRoutes = require('./routes/authRoutes');
+const staticRoutes = require('./routes/staticRoutes');
+
+let otherRoutes = {
 
   /**
    * @api {get} /featured Request featured photographers
@@ -56,151 +61,9 @@ module.exports.routes = {
   'GET /v1/search': {
     controller: 'SearchController',
     action: 'search',   
-  },
-
-  // static page
-  'PUT /v1/static-page/': {
-    controller: 'StaticPageController',
-    action: 'create',
-    skipAssets: 'true',
-  },
-
-
-  'POST /v1/static-page/:id': {
-    controller: 'StaticPageController',
-    action: 'update',
-    skipAssets: 'true',
-  },
-
-  'GET /v1/static-page/:id': {
-    controller: 'StaticPageController',
-    action: 'get',
-    skipAssets: 'true',
-  },
-
-  'DELETE /v1/static-page/:id': {
-    controller: 'StaticPageController',
-    action: 'delete',
-    skipAssets: 'true',
-  },
-
-  // authentication
-  'GET /v1/authorize-user': {
-    controller: 'AuthController',
-    action: 'authorizeUser',
-  },
-
-  'GET /v1/handle-auth': {
-    controller: 'AuthController',
-    action: 'handleAuth'
-  },
-
-  // invitations
-  'POST /v1/invitation/request': {
-    controller: 'InvitationController',
-    action: 'requestInvitationCode'
-  },
-
-  'POST /v1/invitation/submit': {
-    controller: 'InvitationController',
-    action: 'submitInvitation'
-  },
-
-  /**
-   * @api {get} /me Get basic information for logged in user
-   * @apiName Get basic user information
-   * @apiGroup User
-   *
-   * @apiHeader {String} authorization JW token.
-   *
-   * @apiSuccess {Object} Current user basic data.
-   */
-  'GET /v1/me': {
-    controller: 'PhotographerController',
-    action: 'getBasicInfo'
-  },
-
-  /** @api {put} /me Update basic information for logged in user
-   * @apiName Update basic user information
-   * @apiGroup User
-   *
-   * @apiHeader {String} authorization JW token.
-   * @apiParam {String} firstName User first name
-   * @apiParam {String} lastName User last name
-   * @apiParam {Number} lat latitude
-   * @apiParam {Number} lon longitude
-   * @apiParam {String} email User email
-   * @apiParam {String="amateur","professional"} expertise
-   * @apiParam {Boolean} studio
-   * @apiParam {Number} priceRange Price range 1-5
-   * @apiParam {Array} categories Array of category ids (Example: [1, 2])
-   *
-   * @apiSuccess {Object} Current user basic data.
-   * */
-  'PUT /v1/me': {
-    controller: 'PhotographerController',
-    action: 'updateBasicInfo'
-  },
-
-  /**
-   * @api {get} /me/most-liked Get 33 of the most liked user photos
-   * @apiName Most liked photos
-   * @apiGroup User
-   * @apiDescription This endpoint only returns 20 photos at the moment due to application not being reviewed by instagram wizards yet.
-   *
-   * @apiHeader {String} authorization JW token.
-   *
-   * @apiSuccess {Object[]} Array of objects with attributes: id and photo (url)
-   * */
-  'GET /v1/me/most-liked': {
-    controller: 'PhotographerController',
-    action: 'instagramMostLiked'
-  },
-
-  /**
-   * @api {post} /me/update-photos
-   * @apiName Update photos for logged in user
-   * @apiGroup User
-   * @apiDescription Endpoint updates photographers photos
-   *
-   * @apiHeader {String} authorization JW token.
-   * @apiParam {Object[]} photos array of photos
-   * @apiParamExample {json} Request-Example:
-   *    {
-   *      "photos": [
-   *        {
-   *          "id": "312312312",
-   *          "photo": "photo-url"
-   *        }
-   *      ]
-   *    }
-   *
-   * @apiSuccess {Object} Success message
-   *
-   * */
-  'POST /v1/me/update-photos': {
-    controller: 'PhotographerController',
-    action: 'updatePhotos'
-  },
-
-  /**
-   * @api {put} /me/update-photo-category
-   * @apiName Update photo's category
-   * @apiGroup User
-   *
-   * @apiHeader {String} authorization JW token
-   * @apiParam {Object} photo Array of objects with photoId - categoryId pairs
-   * @apiParamExample {json} Request-Example:
-   *    {
-   *      "photoId": "109",
-   *      "categoryId": "1"
-   *    }
-   *
-   * @apiSuccess {Object} Success message
-   * */
-  'PUT /v1/me/update-photo-category': {
-    controller: 'PhotographerController',
-    action: 'updatePhotoCategory'
   }
 
 };
+let routes = {};
+Object.assign(routes, otherRoutes, meRoutes, invitationRoutes, staticRoutes);
+module.exports.routes = routes;
