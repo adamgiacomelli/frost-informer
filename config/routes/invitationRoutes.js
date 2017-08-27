f70c45aa923d9d20de1b200fa14f9d37b4431c0f
Ziga Kerec <ziga.kerec@gmail.com>
2017-08-27 15:58:05 +0200

    updated api documentation

--- config/routes/invitationRoutes.js
+++ config/routes/invitationRoutes.js
@@ -1,10 +1,30 @@
 module.exports = {
 
+  /**
+   * @api {get} /invitation/request
+   * @apiName RequestInvitation
+   * @apiGroup Invitation
+   *
+   * @apiParam {String} firstName
+   * @apiParam {String} lastName
+   * @apiParam {String} email
+   *
+   * @apiSuccess {Object} Success message
+   */
   'POST /v1/invitation/request': {
     controller: 'InvitationController',
     action: 'requestInvitationCode'
   },
 
+  /**
+   * @api {get} /invitation/submit
+   * @apiName SubmitInvitation
+   * @apiGroup Invitation
+   *
+   * @apiParam {String} code invitation code
+   *
+   * @apiSuccess {Object} Success message
+   */
   'POST /v1/invitation/submit': {
     controller: 'InvitationController',
     action: 'submitInvitation'
